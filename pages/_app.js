e7f0a68987c5120a85c5456ba2405a2b04e5f86e
Aljosa Makevic <aljosa100@gmail.com>
2022-09-29 11:46:28 +0200

    gasless mint mvp

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider sdkOptions={{
+      gasless: {
+        openzeppelin: {
+          relayerUrl: process.env.NEXT_PUBLIC_OPENZEPPELIN_URL,
+        },
+      },
+    }}
+    desiredChainId={ChainId.Mumbai}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
